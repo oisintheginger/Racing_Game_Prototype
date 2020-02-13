a16928f28cf123b951da33c49516808c7f77e09a
oisintheginger <32101314+oisintheginger@users.noreply.github.com>
2020-02-13 00:08:08 +0000

    Added Basic Movement Script
    Going to edit how the turning in the player controller works. Will start doing a turn force perpendicular to the forward vector and then turn the object to face the Vector.

---
+++ Racing Prototype/Assets/Script Folder/playerMotion.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMotion : MonoBehaviour
+{
+    Rigidbody pRB;
+    [SerializeField] Vector3 appliedForce;
+    [SerializeField] float maxSpeed, maxForce, accelerationForce, decellerateForce, xZPlaneSpeed, jumpForce, turnSpeed;
+    [SerializeField] bool isGrounded;
+    [SerializeField] Transform groundTransform, slopeTransform;
+
+    Ray groundCheckRay;
+
+    private void Awake()
+    {
+        pRB = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GroundCheck();
+        groundCheckRay = new Ray(groundTransform.position, -transform.up);
+        Motion();
+
+    }
+
+    Vector3 CalculateForce()
+    {
+        return Vector3.zero;
+    }
+
+    void GroundCheck()
+    {
+        RaycastHit rH;
+        if (Physics.Raycast(groundTransform.position, -transform.up, out rH, 0.5f))
+        {
+            if (rH.collider.gameObject.tag == "Ground")
+            {
+                 isGrounded =true;
+            }
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+    void Motion()
+    {
+        pRB.drag = 0;
+        
+        var velocity = pRB.velocity;
+
+        //turning
+        float turning = Input.GetAxis("Horizontal");
+        pRB.AddRelativeTorque(transform.up * turning * (turnSpeed*(maxSpeed/Mathf.Max(10f, velocity.magnitude))));
+
+        //speedClamping
+        
+        if(Input.GetKey(KeyCode.W))
+        {
+
+            xZPlaneSpeed = Mathf.Sqrt((velocity.x * velocity.x) + (velocity.z * velocity.z));
+            if (xZPlaneSpeed < maxSpeed)
+            {
+                pRB.AddForce(transform.forward * accelerationForce,ForceMode.VelocityChange);
+            }
+            if (xZPlaneSpeed >= maxSpeed)
+            {
+                Vector3 normalizedXZMovement = new Vector3(velocity.x / xZPlaneSpeed, velocity.y, velocity.z / xZPlaneSpeed);
+
+                Vector3 xzSpeed = new Vector3(normalizedXZMovement.x * maxSpeed, velocity.y, normalizedXZMovement.z * maxSpeed);
+
+                pRB.velocity = xzSpeed;
+            }
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space)&&isGrounded)
+        {
+            pRB.AddForce(transform.up * jumpForce,ForceMode.VelocityChange);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(slopeTransform.position, transform.forward+slopeTransform.position);
+    }
+}
